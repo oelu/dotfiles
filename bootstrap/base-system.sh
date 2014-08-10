debb5509b7b0451c0974b315ec55c3e8d2374ea5
oschenk <olivier.schenk@united-security-providers.ch>
2014-08-10 10:40:47 +0200

    added aspell

--- bootstrap/base-system.sh
+++ bootstrap/base-system.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update && sudo apt-get dist-upgrade -y
 
-sudo apt-get install lua5.1 zsh python vim tmux netcat tcpdump wireshark git golang asciidoc tshark htop liblua5.1 ltrace build-essential module-assistant virtualbox-guest-additions vim-gnome chromium-browser i3 -y
+sudo apt-get install lua5.1 zsh python vim tmux netcat tcpdump wireshark git golang asciidoc tshark htop liblua5.1 ltrace build-essential module-assistant virtualbox-guest-additions vim-gnome chromium-browser i3 aspell aspell-de -y
