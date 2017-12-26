4029e99e46b9f4cc52a1c2fe0f95b4d70b0786bb
Oelu <olivierschenk@gmail.com>
2017-12-26 12:20:19 +0100

    Added cheat package.

--- bootstrap/python-install.sh
+++ bootstrap/python-install.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get install python-pip -y 
 
-sudo pip install requests docopt virtualenv dkimpy pyexpect dkimpy dnspython httpie tmuxp pydns pymux Django Flask Fabric Markdown cement ciscoconfparse
+sudo pip install requests docopt virtualenv dkimpy pyexpect dkimpy dnspython httpie tmuxp pydns pymux Django Flask Fabric Markdown cement ciscoconfparse cheat
