f696ee6729d11b9995fa169895ac035ca835bd2d
Oelu <olivierschenk@gmail.com>
2018-01-12 13:44:38 +0100

    Adjusted packages.

--- bootstrap/base-system.sh
+++ bootstrap/base-system.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update && sudo apt-get dist-upgrade -y
 
-sudo apt-get install lua5.1 zsh python vim tmux netcat tcpdump wireshark git golang asciidoc tshark htop liblua5.1 ltrace build-essential module-assistant virtualbox-guest-additions vim-gnome chromium-browser i3 cmake python-dev fish -y
+sudo apt-get install zsh python vim tmux netcat tcpdump wireshark git golang asciidoc tshark htop ltrace build-essential module-assistant xfce4 i3 cmake python-dev rofi scrot feh compton lxappearance xfonts-terminus arc-theme -y
