6961f7f9da06de2891b0ba85c954aa8edf97945a
Oelu <olivierschenk@gmail.com>
2018-01-13 20:07:20 +0100

    Added git-gui.

--- bootstrap/base-system.sh
+++ bootstrap/base-system.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update && sudo apt-get dist-upgrade -y
 
-sudo apt-get install zsh python vim tmux netcat tcpdump wireshark git golang asciidoc tshark htop ltrace build-essential module-assistant xfce4 i3 cmake python-dev rofi scrot feh compton lxappearance xfonts-terminus arc-theme xfce4-terminal -y
+sudo apt-get install zsh python vim tmux netcat tcpdump wireshark git golang asciidoc tshark htop ltrace build-essential module-assistant xfce4 i3 cmake python-dev rofi scrot feh compton lxappearance xfonts-terminus arc-theme xfce4-terminal git-gui -y
