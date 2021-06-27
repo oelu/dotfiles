e48f16cbb00c1786e029ef448b2c868d8813e12c
Oelu <osc@x260.local>
2021-06-27 21:07:00 +0200

    Added mtr

--- bootstrap/base-system.sh
+++ bootstrap/base-system.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update && sudo apt-get dist-upgrade -y
 
-sudo apt-get install zsh python vim tmux netcat tcpdump wireshark git htop rofi scrot feh curl emacs python3 keepassx locate chromium-browser git-gui tldr -y
+sudo apt-get install zsh python vim tmux netcat tcpdump wireshark git htop rofi scrot feh curl emacs python3 keepassx locate chromium-browser git-gui tldr mtr -y
